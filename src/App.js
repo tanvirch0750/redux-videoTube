aaf515fa9c6867f210740fcfe0bf634dcd14d21c
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-13 15:35:42 +0600

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Home from './pages/Home';
 import Video from './pages/Video';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      {/* <Home /> */}
-      <Video />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:id" element={<Video />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,13 @@
-import Navbar from '../components/navbar/Navbar';
 import Tags from '../components/tags/Tags';
 import VideoGrid from '../components/grid/VideoGrid';
 import Pagination from '../components/ui/Pagination';
-import Footer from '../components/footer/Footer';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
       <Tags />
       <VideoGrid />
       <Pagination />
-      <Footer />
     </>
   );
 };
