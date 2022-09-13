a9a3a3ac02108b94671988bfd140518bc7f6b2f1
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-13 18:12:39 +0600

    implemented videos feature with redux

---
+++ src/features/videos/videosSlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getVideos } from './videosApi';
+
+const initialState = {
+  videos: [],
+  isLoading: false,
+  isError: false,
+  error: '',
+};
+
+// async thunk
+export const fetchVideos = createAsyncThunk('videos/fetchVideos', async () => {
+  const videos = await getVideos();
+  return videos;
+});
+
+const videoSlice = createSlice({
+  name: 'videos',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchVideos.pending, (state) => {
+        state.isError = false;
+        state.isLoading = true;
+      })
+      .addCase(fetchVideos.fulfilled, (state, action) => {
+        state.isError = false;
+        state.isLoading = false;
+        state.videos = action.payload;
+      })
+      .addCase(fetchVideos.rejected, (state, action) => {
+        state.isError = true;
+        state.isLoading = false;
+        state.videos = [];
+        state.error = action.error?.message;
+      });
+  },
+});
+
+export default videoSlice.reducer;
